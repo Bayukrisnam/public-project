014a31a17b96749b23f830a11a6e619732175319
Bayukrisnam <bkmbayukrisnamukti@gmail.com>
2024-08-13 10:12:34 +0800

    Add files via upload

---
+++ # TUGAS DATA TYPES.py
@@ -0,0 +1,14 @@
+# TUGAS DATA TYPES
+#..............
+
+Number : int = 12
+Number_2 : float = 30.5
+boolean_var = True
+string_var = "My String"
+
+
+if __name__ == '_main_' :
+    print(Number + Number_2)
+    #print(Number + boolean_var)
+    #print(Number + string_var)
+    
