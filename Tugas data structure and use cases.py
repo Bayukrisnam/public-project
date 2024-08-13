189fff99afcc392a4698a5b1ea674b4df5f25721
Bayukrisnam <bkmbayukrisnamukti@gmail.com>
2024-08-13 10:49:07 +0800

    Add files via upload

---
+++ Tugas data structure and use cases.py
@@ -0,0 +1,29 @@
+# TUGAS Data sctructure and use case
+#..............
+
+Number : int = 12
+Number_2 : float = 30.5
+boolean_var = True
+string_var = "My String"
+
+
+my_list = (1,2,3,4,5,'String',[1,2,4],100)
+my_map = {
+    "key_1" = 1,
+    "key_2" = 2
+}
+
+my_map_2 = {
+    "key_3" = 3,
+    "key_4" = 4
+}
+
+
+if __name__ == '_main_' :
+    #print(Number + Number_2)
+    #print(Number + boolean_var)
+    #print(Number + string_var)
+    #print(my_list[-1])
+    #my_list.append(1000)
+    my_map.update[my_map_2]
+    print(my_map)
